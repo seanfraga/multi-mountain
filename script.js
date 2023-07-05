0193cd9db64df414bc0bcbf58af945c788b762f5
Sean Fraga <seanfraga@gmail.com>
2023-07-05 14:57:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 // Load the messages asynchronously from the CSV file
 async function loadMessages() {
-  const response = await fetch('messages.csv');
-  const csvData = await response.text();
-  const messages = Papa.parse(csvData, { header: true, skipEmptyLines: true }).data;
-
-// Calculate the total weight of all messages
-  const totalWeight = messages.reduce((sum, message) => sum + parseInt(message.weight), 0);
-
-// Generate a random number between 0 and the total weight
-  const randomNumber = Math.floor(Math.random() * totalWeight);
-
-// Select a message based on its weight
-  let selectedMessage;
-  for (const message of messages) {
-    if (randomNumber < parseInt(message.weight)) {
-      selectedMessage = message.message;
-      break;
+  try {
+    const response = await fetch('messages.csv');
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    randomNumber -= parseInt(message.weight);
-  }
+    const csvData = await response.text();
+    const messages = Papa.parse(csvData, { header: true, skipEmptyLines: true }).data;
 
-  document.getElementById("message").innerHTML = selectedMessage;
+    // Calculate the total weight of all messages
+    const totalWeight = messages.reduce((sum, message) => sum + parseInt(message.weight), 0);
+
+    // Generate a random number between 0 and the total weight
+    const randomNumber = Math.floor(Math.random() * totalWeight);
+
+    // Select a message based on its weight
+    let selectedMessage;
+    for (const message of messages) {
+      if (randomNumber < parseInt(message.weight)) {
+        selectedMessage = message.message;
+        break;
+      }
+      randomNumber -= parseInt(message.weight);
+    }
+
+    document.getElementById("message").innerHTML = selectedMessage;
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
 }
 
 // Call the loadMessages function when the page is loaded
